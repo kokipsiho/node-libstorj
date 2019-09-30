33bdc770ad85e08834fd01e1621a9de54e483c97
Zoran Joka <zoran.joka@gmail.com>
2019-09-30 12:26:16 +0200

    Update binding.cc

--- binding.cc
+++ binding.cc
@@ -882,7 +882,7 @@ void Environment(const v8::FunctionCallbackInfo<Value>& args) {
     Nan::MaybeLocal<v8::Object> maybeInstance;
     v8::Local<v8::Object> instance;
 
-    v8::Local<v8::Value> argv[] = {};
+    v8::Local<v8::Value> argv[999] = {};
     maybeInstance = Nan::NewInstance(constructor->GetFunction(), 0, argv);
 
     if (maybeInstance.IsEmpty()) {
